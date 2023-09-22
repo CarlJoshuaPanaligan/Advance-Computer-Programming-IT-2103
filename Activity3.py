b9cd3447282b0c01e6cfb5d57394f4aaca9ec78f
CarlJoshuaPanaligan <145734479+CarlJoshuaPanaligan@users.noreply.github.com>
2023-09-22 20:38:54 +0800

    Create Activity3.py

---
+++ Activity3.py
@@ -0,0 +1,25 @@
+a = 0
+while a < 5:
+    print("Advanced Computer Programming")
+    a += 1
+    
+b = -1
+while b < 6:
+    b += 1
+    print(b)
+    
+c = 0
+while c < 10:
+    c += 1
+    print(c)
+    
+d = -1
+while d < 20:
+    d += 1
+    print(d)
+    
+name = input("Enter name: ")
+e = 0
+while e < 4:
+    print(name)
+    e += 1
