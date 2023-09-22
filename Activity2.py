f7feaf31392f734761abc6362eb3d6f1534f5227
CarlJoshuaPanaligan <145734479+CarlJoshuaPanaligan@users.noreply.github.com>
2023-09-22 20:24:18 +0800

    Update Activity2.py

--- Activity2.py
+++ Activity2.py
@@ -1,3 +1,12 @@
+name = input("Enter your father's name: ")
+birthplace = input("Enter your father's birthplace: ")
+birthday = input("Enter your father's birthday: ")
+birthyear = int(input("Enter your father's birthyear: "))
+
+age = 2023 - birthyear
+
+print(name + " is " + str(age) + " years old and lives in " + birthplace + ". His birthday is on " + birthday)
+
 english = int(input("Enter grades for ENGLISH: "))
 filipino = int(input("Enter grades for FILIPINO: "))
 science = int(input("Enter grades for SCIENCE: "))
